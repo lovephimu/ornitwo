ddb18d696fc4a0b303d4573e02bafab3129a2235
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-07-04 17:08:58 +0200

    Add env graphql

--- util/apolloClient.ts
+++ util/apolloClient.ts
@@ -5,7 +5,7 @@ export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: 'http://localhost:3000/api/graphql',
+      uri: `${process.env.GRAPHQL_ENDPOINT}/api/graphql`,
       credentials: 'same-origin',
     }),
   });
