70fc675b6d3f53f510d4a9a843611176f3fc2ea1
lovephimu <127794781+lovephimu@users.noreply.github.com>
2023-07-17 16:56:07 +0200

    Add progress bar

--- app/components/ExploreButton.tsx
+++ app/components/ExploreButton.tsx
@@ -2,19 +2,27 @@
 
 import { Route } from 'next';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function ExploreButton() {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   return (
-    <div className="flex font-mono font-light bg-gray-775 w-full justify-center h-32 items-center md:hidden">
-      <button
-        onClick={() => {
-          router.replace('/explore' as Route);
-        }}
-      >
-        Explore
-      </button>
-    </div>
+    <>
+      <div className="flex font-mono font-light bg-gray-775 w-full justify-center h-32 items-center md:hidden">
+        <button
+          onClick={() => {
+            setLoading(true);
+            router.replace('/explore' as Route);
+          }}
+        >
+          Explore
+        </button>
+      </div>
+      {loading && (
+        <progress className="left-0 top-0 w-screen progress progress-warning absolute" />
+      )}
+    </>
   );
 }
